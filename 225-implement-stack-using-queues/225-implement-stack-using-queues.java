0f1d89233c464d4009f365d14920b5ee2315cfb4
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-05-05 23:02:24 +0530

    Time: 1 ms (22.34%), Space: 41.2 MB (70.90%) - LeetHub

--- 225-implement-stack-using-queues/225-implement-stack-using-queues.java
+++ 225-implement-stack-using-queues/225-implement-stack-using-queues.java
@@ -2,18 +2,18 @@ class MyStack {
     private Queue<Integer> q1;
     private Queue<Integer> q2;
 
-    /** Initialize your data structure here. */
+   
     public MyStack() {
         q1 = new LinkedList<>();
         q2 = new LinkedList<>();
     }
 
-    /** Push element x onto stack. */
+   
     public void push(int x) {
         q1.add(x);
     }
 
-    /** Removes the element on top of the stack and returns that element. */
+  
     public int pop() {
         while (q1.size() > 1) {
             q2.add(q1.poll());
@@ -34,12 +34,12 @@ class MyStack {
         return rst;
     }
 
-    /** Returns whether the stack is empty. */
+   
     public boolean empty() {
         return q1.isEmpty();
     }
 
-    /** Swap the two queues. */
+   
     private void swap() {
         Queue<Integer> temp = q1;
         q1 = q2;
