1f0255e88cbc18cbb98e9b91c22ce66185efd456
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-07-17 16:39:30 +0530

    Time: 138 ms (6.13%), Space: 117 MB (5.81%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,13 +1,20 @@
 class Solution {
     public int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
-        }
-        else{
-            return fib(n-1)+fib(n-2);
-        }
+        // if(n==0){
+        //     return 0;
+        // }
+        // else if(n==1){
+        //     return 1;
+        // }
+        // else{
+        //     return fib(n-1)+fib(n-2);
+        // }
+        /* Dynamic programming solution */
+        int dp[] = new int[n+1];
+        if(n<=1) return n;
+        if(dp[n] != 0)
+            return dp[n];
+        else
+            return dp[n] = fib(n-1) + fib(n-2);
     }
 }
