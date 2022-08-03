20346288102626c2e1c7d21d1cdc556ff94b601d
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-08-03 13:54:45 +0530

    Time: 13 ms (99.90%), Space: 42.9 MB (94.90%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.java
@@ -0,0 +1,55 @@
+class MyCalendar {
+private TreeNode root;
+	public MyCalendar() {
+
+	}
+
+	public boolean book(int start, int end) {
+		if(root==null)
+		{
+			root=new TreeNode(start,end);
+			return true;
+		}
+		return insert(start,end,root);
+	}
+
+	public boolean insert(int start, int end, TreeNode root){
+		if(end<=root.start)
+		{
+			if(root.left==null)
+			{
+				root.left=new TreeNode(start,end);
+				return true;
+			}
+			return insert(start,end,root.left);
+		}
+		if(start>=root.end)
+		{
+			if(root.right==null)
+			{
+				root.right=new TreeNode(start,end);
+				return true;
+			}
+			return insert(start,end,root.right);
+		}
+		return false;
+	}
+
+	class TreeNode {
+		int start;
+		int end;
+		TreeNode left;
+		TreeNode right;
+		public TreeNode(int start, int end)
+		{
+			this.start=start;
+			this.end=end;
+		}
+	}
+}
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar obj = new MyCalendar();
+ * boolean param_1 = obj.book(start,end);
+ */
