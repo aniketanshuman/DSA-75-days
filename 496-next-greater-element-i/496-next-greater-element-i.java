47822157e9e2438a40e275f2fab6e89727574d22
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-08-15 00:00:48 +0530

    Time: 4 ms (82.10%), Space: 44.4 MB (37.39%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+        
+        for (int num : nums) {
+            while (!stack.isEmpty() && stack.peek() < num) {
+                map.put(stack.pop(), num);
+            }
+            stack.push(num);
+        }
+        
+        int[] res = new int[findNums.length];
+        for (int i = 0; i < findNums.length; i++) {
+            res[i] = map.getOrDefault(findNums[i], -1);
+        }
+        return res;
+    }
+}
