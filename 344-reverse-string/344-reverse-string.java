41504ce5c8e310c80492fa5af8ab6399379dc0a7
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-08-21 10:33:21 +0530

    Time: 2 ms (41.31%), Space: 54.6 MB (60.82%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -3,11 +3,14 @@ class Solution {
         int i =0;
         int j = c.length-1;
         while(i<j){
-            char temp = c[i];
-            c[i] = c[j];
-            c[j] = temp;
+          swap(c, i, j);
             i++;
             j--;
         }
-    }    
+    }   
+    public void swap(char[] c, int i, int j){
+        char temp = c[i];
+        c[i] = c[j];
+        c[j] = temp;
+    }
 }
