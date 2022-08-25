a55ccbebc8967ef3564de6c0419e9258ccf8cb13
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-08-25 09:17:19 +0530

    Time: 4 ms (84.52%), Space: 46.7 MB (50.94%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int arr[] = new int[26];
+        for(char i : magazine.toCharArray()){
+          arr[i-'a'] = arr[i - 'a'] + 1;
+        }
+        for(char i : ransomNote.toCharArray()){
+          if(arr[i - 'a'] == 0) return false;
+            else
+                arr[i - 'a'] = arr[i - 'a'] - 1;
+        }
+        return true;
+    }
+}
