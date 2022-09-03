13ea116fcdb2e54997e4b9e6da7580eabd5f3e50
Aniket Anshuman <73903165+aniketanshuman@users.noreply.github.com>
2022-09-04 00:27:26 +0530

    Time: 49 ms (22.11%), Space: 44.1 MB (94.68%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,3 +1,11 @@
+class Pair{
+    int diff;
+    int elem;
+    Pair(int diff, int elem){
+        this.diff = diff;
+        this.elem = elem;
+    }
+}
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>((a,b)->Math.abs(a-x) - Math.abs(b-x) == 0 ? a - b : Math.abs(a-x) - Math.abs(b-x));
@@ -9,5 +17,20 @@ class Solution {
         }
         Collections.sort(result);
         return result;
+        // PriorityQueue<Pair> pq = new PriorityQueue<Pair>(Collections.reverseOrder());
+        // List<Integer> ans = new ArrayList<>();
+        //  // pq.add(new Pair(Math.abs(arr[0]-x), arr[2]));
+        // for(int i=0; i< arr.length; i++){
+        //     pq.add(new Pair(Math.abs(arr[i]-x), arr[i]));
+        //     if(pq.size()>k){
+        //         pq.poll();
+        //     }
+        // }
+        // while(!pq.isEmpty()){
+        //     Pair it = pq.peek();
+        //     ans.add(it.elem);
+        //     pq.poll();
+        // }
+        // return ans;
     }
 }
